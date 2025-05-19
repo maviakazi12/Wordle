c5744c6edc4bf5b8fe0a26f893bd47cc1afaaa3e
maviakazi12 <mavia.kazi@myob.com>
2025-05-19 21:09:54 +1000

    test: write test StartGame_Passes_SecretWord_To_GuessCheck

--- Wordle.Tests/ControllerTests/GameControllerTests.cs
+++ Wordle.Tests/ControllerTests/GameControllerTests.cs
@@ -1,8 +1,9 @@
+using FluentAssertions;
 using JetBrains.Annotations;
 using Wordle.Controller;
-using FluentAssertions;
 using Wordle.Interfaces;
 using Moq;
+using Moq.AutoMock;
 
 
 namespace Wordle.Tests.Controller;
@@ -10,18 +11,38 @@ namespace Wordle.Tests.Controller;
 [TestSubject(typeof(GameController))]
 public class GameControllerTests
 {
+    private (GameController controller, AutoMocker mocker) CreateGameController()
+    {
+        // var gameController = new GameController(mockWordProvider.Object);
+        var mocker = new AutoMocker();
+        var controller = new GameController(
 
+            mocker.Get<IWordProvider>(),
+            mocker.Get<IGuessChecker>()
+        );
+        return (controller, mocker);
+    }
     [Fact]
-    public void StartGame_ReturnsWordProvidedByWordProvider()
+    public void StartGame_Returns_SecretWord_Provided_By_WordProvider()
     {
         //Arrange
-        var mockWordProvider = new Mock<IWordProvider>();
-        mockWordProvider.Setup(a => a.SecretWord).Returns("plate");
-        var gameController = new GameController(mockWordProvider.Object);
+        var (controller, mocker) = CreateGameController();
+        mocker.GetMock<IWordProvider>().Setup(a => a.SecretWord).Returns("plate");
         //Act
-        gameController.StartGame();
-        var word = gameController.SecretWord;
+        controller.StartGame();
+        var word = controller.SecretWord;
         //Assert
         word.Should().Be("plate");
+    }
+
+    [Fact]
+    public void StartGame_Passes_SecretWord_To_GuessCheck()
+    {
+        //Arrange
+        var (controller, mocker) = CreateGameController();
+        //Act
+        controller.StartGame();
+        //Assert
+        mocker.GetMock<IGuessChecker>().Verify(a => a.CompareWords("plate"), Times.Once);
     }
 }
