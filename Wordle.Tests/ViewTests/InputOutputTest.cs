5351bed94f8e3cb90c30bcde372558e3c02cec9f
maviakazi12 <mavia.kazi@myob.com>
2025-05-20 11:32:57 +1000

    test: write IIO test for welcome message

--- Wordle.Tests/ViewTests/InputOutputTest.cs
+++ Wordle.Tests/ViewTests/InputOutputTest.cs
@@ -1,15 +1,24 @@
+using FluentAssertions;
 using JetBrains.Annotations;
 using Wordle.View;
 
-namespace Wordle.Tests.View;
+namespace Wordle.Tests.ViewTests;
 
 [TestSubject(typeof(InputOutput))]
 public class InputOutputTest
 {
 
     [Fact]
-    public void METHOD()
+    public void Welcome_Should_Show_Welcome_Message_To_Console()
     {
-        
+        //Arrange
+        var stringWriter = new StringWriter();
+        Console.SetOut(stringWriter);
+        var inputOutput = new InputOutput();
+        //Act
+        InputOutput.Welcome();
+        var output = stringWriter.ToString();
+        //Assert
+        output.Should().Contain("Welcome To Wordle");
     }
 }
