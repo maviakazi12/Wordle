b2a138f1ad664d68caaa298d69f0b168aaa0c17a
maviakazi12 <mavia.kazi@myob.com>
2025-05-20 20:59:46 +1000

    feat: injected WordValidator dependency

--- Wordle/View/InputOutput.cs
+++ Wordle/View/InputOutput.cs
@@ -5,6 +5,12 @@ namespace Wordle.View;
 
 public class InputOutput : IInputOutput
 {
+    private IWordValidator _wordValidator;
+
+    public InputOutput(IWordValidator wordValidator)
+    {
+        _wordValidator = wordValidator;
+    }
     public void Welcome()
     {
         Console.WriteLine(PromptMessages.Welcome);
@@ -17,7 +23,10 @@ public class InputOutput : IInputOutput
         {
             Console.WriteLine(PromptMessages.GetUserInput);
             playerInput = Console.ReadLine()?.ToLower() ?? "";
-        } while (string.IsNullOrWhiteSpace(playerInput));
+
+            if (playerInput.Length != 5) Console.WriteLine(PromptMessages.ValidWordLengthMessage);
+            if (!_wordValidator.IsWordValid(playerInput)) Console.WriteLine(PromptMessages.InvalidWord);
+        } while (string.IsNullOrWhiteSpace(playerInput) || playerInput.Length != 5 || !_wordValidator.IsWordValid(playerInput));
 
         return playerInput;
     }
